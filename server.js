d61e1d6be334c985a39f3f93530546ae527c3025
Him7110 <124056530+Him7110@users.noreply.github.com>
2024-10-20 00:14:00 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+// console.log(app.get('env'));
+// console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connecton successful!'));
+
+
+const port = 3000;
+const server = app.listen(port, ()=> {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+})
